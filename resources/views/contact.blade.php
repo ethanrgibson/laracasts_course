162f69348d32e3d84ba300be77c7c45af49b342c
ethanrgibson <ethanrgibson33@gmail.com>
2025-05-05 21:08:07 -0600

    injecting tailwind

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Contact</title>
-</head>
-<body>
-<h1>Contact me ok ok</h1>      
-</body>
-</html>
+<x-layout>
+    <h1>Hello from contact page</h1>
+</x-layout>
