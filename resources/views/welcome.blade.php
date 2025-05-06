a5681f57c21c367eb5a10a635ea15acc5295bfe0
ethanrgibson <ethanrgibson33@gmail.com>
2025-05-05 20:43:52 -0600

    creating component and layout file

--- resources/views/welcome.blade.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        
-    </head>
-    <body class="bg-[#FDFDFC] dark:bg-[#0a0a0a] text-[#1b1b18] flex p-6 lg:p-8 items-center lg:justify-center min-h-screen flex-col">
-        <h1>what up world wassup danger</h1>
-    </body>
-</html>
