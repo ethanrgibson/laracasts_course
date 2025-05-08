ddb796dd8cbd84381fa2ba548c4dd27933eb022b
ethanrgibson <ethanrgibson33@gmail.com>
2025-05-08 14:39:36 -0600

    getting job by id

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use \Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,19 +12,50 @@ Route::get('/jobs', function () {
         'greeting' => 'Hello',
         'jobs' => [
             [
+                'id' => 1,
                 'title' => 'director',
                 'salary' => '$50,000'
             ],
             [
+                'id' => 2,
                 'title' => 'Big Dawg',
                 'salary' => '$500,000'
             ],
             [
+                'id' => 3,
                 'title' => 'Street Sweeper',
                 'salary' => '$5,000,000'
             ]
         ]
     ]);
+});
+
+Route::get('/jobs/{id}', function($id){
+    
+$jobs = [
+            [
+                'id' => 1,
+                'title' => 'director',
+                'salary' => '$50,000'
+            ],
+            [
+                'id' => 2,
+                'title' => 'Big Dawg',
+                'salary' => '$500,000'
+            ],
+            [
+                'id' => 3,
+                'title' => 'Street Sweeper',
+                'salary' => '$5,000,000'
+            ]
+
+        ];
+
+        $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
+
+        dd($job );
+
+    return view('job', ['job' => $job ]);
 });
 
 Route::get('/contact', function(){
