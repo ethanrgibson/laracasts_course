655922e2444e8179f5d203eecb972f4071b3d627
ethanrgibson <ethanrgibson33@gmail.com>
2025-05-08 16:48:28 -0600

    using eloquent to query db

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,41 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Arr;
 
-class Job {
-  public static function all(): array
-  {
-      return [
-          [
-              'id' => 1,
-              'title' => 'director',
-              'salary' => '$50,000'  
-          ],
-          [
-              'id' => 2,
-              'title' => 'Big Dawg',
-              'salary' => '$500,000'
-          ],
-          [
-              'id' => 3,
-              'title' => 'Street Sweeper',
-              'salary' => '$5,000,000'
-          ]];
-  }
+class Job extends Model
+{
 
-  public static function find(int $id): array
-  {
-  
-    $job = Arr::first(static::all(), fn($job) => $job['id'] == $id); 
-
-    if(! $job)
-    {
-      abort(404);
-    };
-
-    return $job; 
-  }
-
-
+  protected $table = 'job_listings';
 }
