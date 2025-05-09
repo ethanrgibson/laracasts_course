d3b137809d5e03ef26d7467971ce3d980b8a6bd7
ethanrgibson <ethanrgibson33@gmail.com>
2025-05-09 12:56:26 -0600

    cascade on delete working properly

--- database/migrations/2025_05_09_183506_create_tags_table.php
+++ database/migrations/2025_05_09_183506_create_tags_table.php
@@ -19,7 +19,7 @@ return new class extends Migration
 
         Schema::create('job_tag', function (Blueprint $table) {
             $table->id();
-            $table->foreignIdFor(\App\Models\Job::class,'job_listing_id') ->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(\App\Models\Job::class, 'job_listing_id')->constrained()->cascadeOnDelete();
             $table->foreignIdFor(\App\Models\Tag::class)->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
@@ -31,6 +31,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('tags');
-        Schema::dropIfExists('job_tags');
+        Schema::dropIfExists('job_tag');
     }
 };
