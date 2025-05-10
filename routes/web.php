6741e9b4e99fef4f48b2fd32c4a8e8e7c103c4bb
ethanrgibson <ethanrgibson33@gmail.com>
2025-05-10 15:56:32 -0600

    creating a job with form

--- routes/web.php
+++ routes/web.php
@@ -19,10 +19,20 @@ Route::get('/jobs/create', function () {
     return view('jobs.create');
 });
 
-Route::get('/jobs/{id}', function ($id) {
+Route::get('/jobs/{id}', function ($id) { 
     $job = Job::find($id);
 
     return view('jobs.show', ['job' => $job]);
+});
+
+Route::post('/jobs', function () {
+
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
+    return redirect('/jobs');
 });
 
 
